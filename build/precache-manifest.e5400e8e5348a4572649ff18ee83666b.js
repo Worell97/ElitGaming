a3005946d42c221a058e1e9a9049e14a3863d4ff
Oscar W. Filho <oscar.filho@eco.centauro>
2020-07-29 13:28:03 -0300

    aula 3-4

---
+++ build/precache-manifest.e5400e8e5348a4572649ff18ee83666b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7f30e4623fde0df2b8f64f9a4d2c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0d05118d5dc95226640",
+    "url": "/static/css/main.c6186f3e.chunk.css"
+  },
+  {
+    "revision": "db1889cf5dfee0380469",
+    "url": "/static/js/2.6f206d49.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6f206d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d05118d5dc95226640",
+    "url": "/static/js/main.259b6471.chunk.js"
+  },
+  {
+    "revision": "3b367380bb828577acf7",
+    "url": "/static/js/runtime-main.2a9157d7.js"
+  },
+  {
+    "revision": "65e6b121ddeb619170b7dc35adf5e18f",
+    "url": "/static/media/Logo.65e6b121.png"
+  }
+]);
